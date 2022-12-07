a93d335f4b4b6d440b562793cd42b637f47d6151
donal <donalshortt@donals.eu>
2022-12-07 21:42:19 +0000

    initial

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import axios from "./plugins/axios";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(axios, {
+	baseUrl: "http://127.0.0.1:3080/",
+})
+
+app.mount("#app");
