91ca1dd860573ca88f3ee596dfdd070af7f2dd66
donalshortt <donalshortt@donals.eu>
2023-10-28 01:22:55 +0200

    working on adding admin panel

---
+++ src/views/AdminView.vue
@@ -0,0 +1,31 @@
+<script>
+import TopBar from "../components/TopBar.vue"
+
+export default {
+	name: 'Admin',
+	data() {
+	},
+	methods: {
+	},
+	components: {
+		TopBar,
+	},
+	mounted() {
+	}
+}
+
+</script>
+
+<template>
+	<div id="admin">
+	
+	<TopBar></TopBar>
+	
+	<h1>BIGGGGG TEESET</h1>
+
+	</div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,73 @@
-<script setup lang="ts">
+<script>
+import Card from "../components/Card.vue"
+import TopBar from "../components/TopBar.vue"
+import BarChart from "../components/BarChart.vue"
+import Line from "../components/LineChart.vue"
+
+export default {
+	name: 'Home',
+	data() {
+		return {
+			date: "0.0.0",
+			name: "name_unset",
+			id: "42c0ff06-1d95-4ef7-bad5-d8e28fa2473b",
+			players: []
+		}
+	},
+	methods: {
+		async fetchGameData() {
+			return await this.$axios.get('/api/game_data', { params: { id: this.id } });
+		},
+
+		setGameState() {
+			this.fetchGameData().then((response) => {
+				console.log("Fetching game data!");
+				this.date = response.data.date;
+				this.name = response.data.name;
+				this.id = response.data.id;
+				this.players = response.data.players;
+			})
+		},
+	},
+	components: {
+		Card,
+		TopBar,
+		BarChart,
+		Line
+	},
+	mounted() {
+		this.setGameState();
+
+		let updateData = () => {
+			this.setGameState();
+			setTimeout(updateData, 15000);
+		};
+
+		setTimeout(updateData, 15000);
+	}
+}
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<div id="main">
+	
+	<TopBar></TopBar>
+
+	<div id="card_container">
+		<Card v-for="player in this.players" :ign=player.igns[0] :score=player.score :tag=player.tag :key=player.tag />
+	</div>
+
+	<BarChart ref="barchart" :players=this.players />
+	<Line ref="linechart" :players=this.players :key=this.date />
+
+	</div>
 </template>
+
+<style scoped>
+
+#card_container {
+	text-align: center;
+}
+
+</style>
