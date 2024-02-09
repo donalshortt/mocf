939f9ae7036e8c920de1f7d50c5c6df675750705
donalshortt <donalshortt@donals.eu>
2024-02-09 15:23:52 +0100

    changed title of webpage

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,37 +3,23 @@
 
 <template>
 	<div id="topbar">
-		<h1 id="title"><span id="royal">Royal</span> Rumble</h1>
+		<h1 id="title">MoC</h1>
 	</div>
 </template>
 
 <style scoped>
+
 #topbar {
 	margin: 0;
 	width: 100%;
 	height: 5vh;
+	padding-top: 0.2em;
 	background-color: #1a1a1a;
 }
 
 #title {
 	margin: 0 0 0 1em;
 	color: var(--off-white);
-}
-
-#royal {
-	background-image: linear-gradient(
-		to right,
-		#462523 0,
-       	#cb9b51 22%, 
-		#f6e27a 45%,
-		#f6f2c0 50%,
-		#f6e27a 55%,
-		#cb9b51 78%,
-		#462523 100%
-	);
-   	color:transparent;
-   	background-clip: text;
-   	-webkit-background-clip:text;
 }
 
 </style>
