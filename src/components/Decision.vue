b74b25299e497b4348f5182e557ba2569c19158e
donalshortt <donalshortt@donals.eu>
2024-04-09 17:39:12 +0200

    feat(app): Admin can make decisions

---
+++ src/components/Decision.vue
@@ -0,0 +1,122 @@
+<script>
+	import BigButton from './BigButton.vue'
+	import { store } from "../store.js"
+	import axios from '../axios-config'
+
+	export default {
+		name: 'Decision',
+		data() {
+			return {
+				prevDecisionState: '',
+				decisionState: 'IsNewIGN',
+				old_ign: null,
+				selectedItem: null,
+				isDroppedDown: false,
+				players_l: store.players,
+				confirmButtonText: 'Confirm',
+			}
+		},
+		props: ['options', 'ign', 'question', 'decKey'],
+		components: { BigButton },
+		methods: {
+			decideIsNewIGN(option) { 
+				switch (option) {
+					case 'New Player':
+						this.decisionState = 'ConfirmNewPlayer'
+						break;
+					case 'New IGN':
+						this.decisionState = 'SelectOldIGN'
+						break;
+				}
+				this.prevDecisionState = 'IsNewIGN'
+			},
+			toggleDropdown() {
+				this.isDroppedDown = !this.isDroppedDown;
+			},
+			selectOldIGN(old_ign) {
+				this.decisionState = 'ConfirmOldIGN'
+				this.old_ign = old_ign
+				this.prevDecisionState = 'SelectOldIGN'
+			},
+			confirmOldIGN() {
+				axios.post('/api/decide', { 
+					key: this.decKey,
+					id: store.id,
+					decision: 'newIGN',
+					old_ign: this.old_ign
+				})
+
+				console.log("button text: ", this.confirmButtonText);
+				this.confirmButtonText = 'Confirmed!';
+				console.log("button text: ", this.confirmButtonText);
+			},
+			confirmNewPlayer() {
+				axios.post('/api/decide', {
+					key: this.key,
+					id: store.id,
+					decision: 'newPlayer'
+				})
+
+				console.log("button text: ", this.confirmButtonText);
+				this.confirmButtonText = 'Confirmed!';
+				console.log("button text: ", this.confirmButtonText);
+			}
+		},
+	}
+
+</script>
+
+<template>
+	<div v-if="this.decisionState == 'IsNewIGN'" class="decision">
+		<h2>{{ question }}</h2>
+		<h3>{{ ign }}</h3>
+		<BigButton @click="decideIsNewIGN(option)" :label="option" v-for="option in options" :key=option />
+	</div>
+
+	<div v-if="this.decisionState == 'SelectOldIGN'" class="decision">
+		<h2>Select old IGN for</h2>
+		<h3>{{ ign }}</h3>
+		<div id="customDropdown" class="buttons" @click="toggleDropdown">
+			<BigButton label="Select" />
+			<div class="dropdown-content" :class="{'active': isDroppedDown}">
+				<a href="#" class="dropdown-item" 
+					v-for="player in players_l" 
+					:key="player.ign" 
+					@click.prevent="selectOldIGN(player.ign)">
+					{{ player.ign }}
+				</a>
+			</div>
+		</div>
+	</div>
+
+	<div v-if="this.decisionState == 'ConfirmOldIGN'" class="decision">
+		<h2>Confirm</h2>
+		<h3>Old IGN: <i>{{ old_ign }}</i></h3>
+		<h3>New IGN: <i>{{ ign }}</i></h3>
+		<BigButton @click="confirmOldIGN()" :label="confirmButtonText"/>
+	</div>
+
+	<div v-if="this.decisionState == 'ConfirmNewPlayer'" class="decision">
+		<h2>Confirm</h2>
+		<h3>New player</h3>
+		<h3>{{ ign }}</h3>
+		<BigButton @click="confirmNewPlayer()" :label="confirmButtonText" />
+	</div>
+</template>
+
+<style scoped>
+
+#decision {
+	margin: auto;
+	width: 90%;
+	height: 20%;
+	border-top: 6px groove rgba(0, 0, 0, .40);
+	padding: .25em;
+}
+
+button {
+	cursor: pointer;
+	margin: 1em;
+}
+
+</style>
---
+++ src/components/Inbox.vue
@@ -0,0 +1,64 @@
+<script>
+	import Decision from './Decision.vue'
+	import axios from '../axios-config'
+
+	import { store } from "../store.js"
+
+	export default {
+		name: 'Inbox',
+		data() {
+			return {
+				store,
+				decisions: []
+			}
+		},
+		methods: {
+			async getDecisions() {
+				console.log("getting decisions");
+				const decisions = await axios.get('/api/decisions', { params: { id: store.id }});
+				this.decisions = decisions.data;
+				console.log(decisions.data);
+			},
+			pollData() {
+				this.getDecisions();
+				setTimeout(this.pollData, 15000);
+			}
+		},
+		mounted() {
+			this.$nextTick(() => {
+				this.pollData();
+			})
+		},
+		components: {
+			Decision
+		},
+	}
+</script>
+
+<template>
+	<div id="inbox">
+		<h1>Inbox</h1>
+
+		<Decision ref="decision" v-for="decision in decisions" :ign=decision.ign :question=decision.question :options=decision.options :decKey=decision.key :key=decision.key />
+	</div>
+</template>
+
+<style scoped>
+	
+#inbox {
+	background-color: rgba(0, 0, 0, .40);
+	backdrop-filter: blur(5px);
+	text-align: center;
+	color: var(--off-white);
+	position: fixed;
+	top: 4em;
+	right: 2em;
+	height: 90vh;
+	width: 20vw;
+	-webkit-box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+	-moz-box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+	box-shadow: 10px 10px 29px 0px rgba(0,0,0,0.75);
+	z-index: 100;
+	border-radius: .5em;
+}
+</style>
