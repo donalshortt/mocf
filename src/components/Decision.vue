82119b2328c163902fc668c34dc6111fa729f9d5
donal shortt <donalshortt@donals.eu>
2024-06-04 11:59:48 +0200

    working on decision flow

--- src/components/Decision.vue
+++ src/components/Decision.vue
@@ -2,16 +2,18 @@
 	export default {
 		name: 'Decision',
 		data() {
+			retun {
+				decisionState: 'IsNewIGN',
+			}
 		},
 		props: ['ign', 'question', 'options'],
-		components: {
-		},
+		components: {},
 		methods: {
 			decide(option) {
-				if (option == 'New Player') {
-					axios.post('/api/decide', { id: store.id, ign: this.ign, decision: 'newIGN' })
-				} else {
-					
+				if (option == "SelectOldIGN") {
+					this.decisionState = "SelectOldIGN";
+				} else if (option == "Confirm") {
+					this.decisionState = "Confirm"
 				}
 			}
 		},
@@ -20,14 +22,40 @@
 </script>
 
 <template>
-	<div id="decision">
+	<div v-if="this.decisionState == 'IsNewIGN'" class="decision">
 		<h2>{{ question }}</h2>
 		<h3>{{ ign }}</h3>
-		<div id="options">
+		<div class="options">
 			<button @click="onSelect" v-for="option in options" :key=option class="button-82-pushable" role="button">
 				<span class="button-82-shadow"></span>
 				<span class="button-82-edge"></span>
-				<span id="login" class="button-82-front text">
+				<span class="button-82-front text login">
+					{{ option }}
+				</span>
+			</button>
+		</div>
+	</div>
+
+	<div v-if="this.decisionState == 'SelectOldIGN'" class="decision">
+		<h2>{{ question }}</h2>
+		<h3>{{ ign }}</h3>
+			<button @click="onSelect" v-for="option in options" :key=option class="button-82-pushable" role="button">
+				<span class="button-82-shadow"></span>
+				<span class="button-82-edge"></span>
+				<span class="button-82-front text login">
+					{{ option }}
+				</span>
+			</button>
+	</div>
+
+	<div v-if="this.decisionState == 'Confirm'" class="decision">
+		<h2>{{ question }}</h2>
+		<h3>{{ ign }}</h3>
+		<div class="options">
+			<button @click="onSelect" v-for="option in options" :key=option class="button-82-pushable" role="button">
+				<span class="button-82-shadow"></span>
+				<span class="button-82-edge"></span>
+				<span class="login" class="button-82-front text login">
 					{{ option }}
 				</span>
 			</button>
